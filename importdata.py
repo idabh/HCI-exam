f218638ca047d4fb3d8418ec37f94727166b366e
KiriKoppelgaard <43064671+KiriKoppelgaard@users.noreply.github.com>
2022-05-11 15:44:54 +0200

    kiri

---
+++ importdata.py
@@ -0,0 +1,16 @@
+import pandas as pd 
+
+#Read csv
+df = pd.read_csv('Data/applicants250.csv',na_values=['a','b'])
+
+#create overview of columns
+df.columns
+
+#manipulate cols
+
+tags = 
+df['tag'] = tags
+
+
+
+
