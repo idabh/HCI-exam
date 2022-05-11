cccf00f451950df3e7319f129b42bce5f1fa5815
KiriKoppelgaard <43064671+KiriKoppelgaard@users.noreply.github.com>
2022-05-11 15:12:26 +0200

    adding page 2

---
+++ Page2.py
@@ -0,0 +1,41 @@
+import streamlit as st
+from streamlit_option_menu import option_menu
+
+# 1=sidebar menu, 2=horizontal menu, 3=horizontal menu w/ custom menu
+EXAMPLE_NO = 2
+
+def streamlit_menu(example=1):
+    if example == 2:
+            # 2. horizontal menu w/o custom style
+            selected = option_menu(
+                menu_title=None,  # required
+                options=["All", "Yes", "Maybe", "No"],  # required
+                icons=["circle", "check-circle", "question-circle", "x-circle"],  # optional
+                menu_icon="cast",  # optional
+                default_index=0,  # optional
+                orientation="horizontal",
+                styles={
+                    "container": {"padding": "0!important", "background-color": "#fafafa"},
+                    #"icon": {"color": "orange", "font-size": "25px"},
+                    "nav-link": {
+                    #    "font-size": "25px",
+                        "text-align": "left",
+                        "margin": "0px",
+                        "--hover-color": "#eee",
+                    },
+                    "nav-link-selected": {"background-color": 'darkgrey'},
+                }
+            )
+            return selected
+
+selected = streamlit_menu(example=EXAMPLE_NO)
+if selected == "All":
+    st.title(f"You have selected {selected}")
+
+if selected == "Yes":
+    st.title(f"You have selected {selected}")
+
+if selected == "Maybe":
+    st.title(f"You have selected {selected}")
+if selected == "No":
+    st.title(f"You have selected {selected}")
