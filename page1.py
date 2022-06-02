3cb87596aa0b9522b3e3eadf30fbd6fbbb545fa8
thearol <thearollesloth@hotmail.com>
2022-06-02 15:00:43 +0200

    thea thrusday

--- page1.py
+++ page1.py
@@ -6,12 +6,12 @@ import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
 from src.utils import *
+
+#PAGE SETUP #######################################################
 #from streamlit_option_menu import option_menu
 st.write('<style>div.Widget.row-widget.stRadio > div{flex-direction:row;}</style>', unsafe_allow_html=True)
-# 1=sidebar menu, 2=horizontal menu, 3=horizontal menu w/ custom menu
 # Remove whitespace from the top of the page and sidebar
-#st.write('<style>div.block-container{padding-top:2rem;}</style>', unsafe_allow_html=True)
-
+st.write('<style>div.block-container{padding-top:2rem;}</style>', unsafe_allow_html=True)
 #condense the space
 padding = 0
 st.markdown(f""" <style>
@@ -24,26 +24,31 @@ st.markdown(f""" <style>
 
 #define style
 local_css("/Users/thearolskovsloth/Documents/MASTERS_I_COGSCI/second_sem/HCI/HCI-exam/styles.css")
+
+########################
+
 #load data
 df = pd.read_csv('/Users/thearolskovsloth/Documents/MASTERS_I_COGSCI/second_sem/HCI/HCI-exam/Data/applicants200.csv')
-education_rank = {"high school":1, "bachelor":2, "masters":3, "phd":4}
+education_rank = {"high school":1, "bachelor":2, "masters":3, "phd":4, "postdoc":5}
 
+
+# DEFINE SIDEBAR #####################
 with st.sidebar:
     st.title("STEP 1: Screening")
     st.header("Filters")
-    st.markdown("Narrow down the applicants by manipulating the filters below")
+    #st.markdown("Narrow down the applicants by manipulating the filters below")
     
-    education_level = st.select_slider("Minimum education level", options=["high school", "bachelor", "masters", "phd"],  help="choose minimum education level needed for this position")
-
+    education_level = st.select_slider("Minimum education level", options=["high school", "bachelor", "masters", "phd", "postdoc"],  help="choose minimum education level needed for this position")
         
-    python_skills = st.slider("Python skills", min_value=0, max_value=5, value=0, step=1, format=None, key="bachelors", help="The performances of the candidates ", on_change=None, args=None, kwargs=None,  disabled=False)
+    python_skills = st.slider("Python skills", min_value=0, max_value=100, value=0, step=1, format=None,  help="The performances of the candidates ", on_change=None, args=None, kwargs=None,  disabled=False)
 
     fac1 = st.slider('Select value fac1', 0,10,1)
     fac2 = st.slider('Select value fac2',0,10,1)
     
-    exp_needed = st.radio("Experience", options= ["Yes", "No"])
+    exp = st.slider("Years exp", min_value=0, max_value=50, value=0, step=1, format=None,  help="The performances of the candidates ", on_change=None, args=None, kwargs=None,  disabled=False)
+    #exp_needed = st.radio("Experience", options= ["Yes", "No"])
     
-    academic_prof = st.multiselect("Academic profile", options=["sciences", "engineering", "arts"], default=["sciences", "engineering", "arts"], key=None, help="Check all the relevant profiles for this position", on_change=None, args=None, kwargs=None, disabled=False)
+    #academic_prof = st.multiselect("Academic profile", options=["sciences", "engineering", "arts"], default=["sciences", "engineering", "arts"], key=None, help="Check all the relevant profiles for this position", on_change=None, args=None, kwargs=None, disabled=False)
 
 
 #process bar
@@ -52,7 +57,7 @@ process_bar = st.progress(25)
 #chosen applicants
 temp_df = df.loc[
     (df['Python_score'] >= python_skills) & 
-    (df['Faculty'].isin(academic_prof)) & 
+    (df['Years_experience'] >= exp) & 
     (df['Education_level'].isin([k for k in education_rank.keys() if education_rank[k] >= education_rank[education_level]]))
     ]
 
@@ -60,92 +65,73 @@ temp_df = df.loc[
 
 remaining_app = len(temp_df)
 diff_app = f"{(remaining_app)-(len(df))} applicants since initial something"
-#st.metric("applicants", remaining_app, delta= diff_app, delta_color="normal")
-
-counting = f'<div data-testid="stMetricValue" class="css-1xarl3l e16fv1kl2"> <div class="css-1ht1j8u e16fv1kl0"> <span style="font-size:20px;"> There are  </span> <span <h1 style="text-align: center;font-size:150px;">  {remaining_app} </h1> </span><span style="font-size:20px;">applicants remaining</span></div></div>'
+counting = f'<div data-testid="stMetricValue" class="css-1xarl3l e16fv1kl2"> <div class="css-1ht1j8u e16fv1kl0"> <span style="font-size:20px;"> &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;There are  &nbsp;&nbsp;</span> <span <h1 style="text-align: center;font-size:100px;">  {remaining_app} </h1> </span><span style="font-size:20px;">applicants remaining</span></div></div>'
 st.markdown(counting, unsafe_allow_html=True)
-
 
 #RADAR##############################################################################################
 
 
-temporal_data = pd.DataFrame(dict(
+radar_data = pd.DataFrame(dict(
     r=[python_skills/10,
-       education_level,
+       education_rank[education_level]*2,
        fac1,
-       fac2],
+       fac2,
+       exp/5],
     theta=['python skills','education_level','factor1',
-           'factor2']))
+           'factor2', 'experience'],
+    color = ["#E4FF87", '#709BFF', '#719BFF', '#FFAA70', '#B6FFB4']))
+
 
 def radar_chart(data):  
-    df = temporal_data
-    fig = px.line_polar(df, r='r', theta='theta', line_close=True)
+    
+    fig = px.line_polar(data, r='r', theta='theta', line_close=True, template='ggplot2', range_r=[0,10], width=600, height=600)
     st.write(fig)
-#chosen_var = st.radio("Select a variable", options= ['education_level','python skills','minimum education', 'thermal stability'])
-#
-radar_chart(temp_df)
 
-st.write('<style>div.Widget.row-widget.stRadio > div{flex-direction:row;}</style>', unsafe_allow_html=True)
+radar_chart(radar_data)
+
+#bar radar
+
+def radar_bar(data):
+    
+    fig1 = px.bar_polar(data, r='r', theta='theta',color='color', template='ggplot2', range_r=[0,10], width=600, height=600)
+    fig1.update_traces(opacity=0.5, selector=dict(type='barpolar')) 
+    st.write(fig1)
+radar_bar(radar_data)
 
 
 
+
+
+import plotly.graph_objects as go
+
+categories = ['python skills','education_level','factor1','factor2', 'experience']
+categories = [*categories, categories[0]]
+
+restaurant_1 = [4, 4, 5, 4, 3]
+
+restaurant_1 = [*restaurant_1, restaurant_1[0]]
+
+
+
+fig = go.Figure(
+    data=[
+        go.Scatterpolar(r=restaurant_1, theta=categories,fill='toself', name='Restaurant 1'),
+        go.Barpolar(r=restaurant_1, theta=categories, name='Restaurant 1',opacity=0.6)
+    ],
+    layout=go.Layout(
+        title=go.layout.Title(text='Restaurant comparison'),
+        polar={'radialaxis': {'visible': True}},
+        showlegend=True
+    )
+)
+
+st.write(fig)
 
 #show data frame
-st.dataframe(data=temp_df, width=None, height=None)
+#st.dataframe(data=temp_df, width=None, height=None)
 
 #BUTTON##############################################################################################
-#next_page = '<button kind="primary" class="css-9dpgwh edgvbvh1" {background-color: #4CAF50;}  >Next</button>'
-next_page = '''
-<html>
-<head>
-<style>
-.button {
-  position: relative;
-  background-color: #4CAF50;
-  border: none;
-  font-size: 28px;
-  color: #FFFFFF;
-  padding: 20px;
-  width: 200px;
-  text-align: center;
-  transition-duration: 0.4s;
-  text-decoration: none;
-  overflow: hidden;
-  cursor: pointer;
-}
-
-.button:after {
-  content: "";
-  background: #f1f1f1;
-  display: block;
-  position: absolute;
-  padding-top: 300%;
-  padding-left: 350%;
-  margin-left: -20px !important;
-  margin-top: -120%;
-  opacity: 0;
-  transition: all 0.8s
-}
-
-.button:active:after {
-  padding: 0;
-  margin: 0;
-  opacity: 1;
-  transition: 0s
-}
-</style>
-</head>
-<body>
-
-<h2>Animated Button - Ripple Effect</h2>
-
-<button class="button">Click Me</button>
-
-</body>
-</html>
-
-
-'''
+next_page = '<button kind="primary" class="css-9dpgwh edgvbvh1" {background-color: #4CAF50;}  >Next</button>'
 
 left_column, mid_column, right_column = st.columns(3)
 with right_column:
@@ -154,50 +140,3 @@ with right_column:
         st.markdown("hello there")
 #st.balloons()
 #st.snow()
-
-
-
-'''
-
-
-#ISOTYPE###############################################################################################
-
-# code for displaying multiple images in one figure
-import altair as alt
-
-#data = pd.DataFrame([dict(id=i) for i in range(0, len(temp_df))])
-data = pd.DataFrame([dict(id=i) for i in random.sample(range(1,10000),len(temp_df))])
-
-person = (
-    "M1.7 -1.7h-0.8c0.3 -0.2 0.6 -0.5 0.6 -0.9c0 -0.6 "
-    "-0.4 -1 -1 -1c-0.6 0 -1 0.4 -1 1c0 0.4 0.2 0.7 0.6 "
-    "0.9h-0.8c-0.4 0 -0.7 0.3 -0.7 0.6v1.9c0 0.3 0.3 0.6 "
-    "0.6 0.6h0.2c0 0 0 0.1 0 0.1v1.9c0 0.3 0.2 0.6 0.3 "
-    "0.6h1.3c0.2 0 0.3 -0.3 0.3 -0.6v-1.8c0 0 0 -0.1 0 "
-    "-0.1h0.2c0.3 0 0.6 -0.3 0.6 -0.6v-2c0.2 -0.3 -0.1 "
-    "-0.6 -0.4 -0.6z"
-)
-
-fig1=alt.Chart(data).transform_calculate(
-    row="ceil(datum.id/10)"
-).transform_calculate(
-    col="datum.id - datum.row*10"
-).mark_point(
-    filled=True,
-    size=50/len(temp_df)*100
-).encode(
-    x=alt.X("col:O", axis=None),
-
-    y=alt.Y("row:O", axis=None),
-    opacity=alt.value(0.4),
-    shape=alt.ShapeValue(person)
-).properties(
-    width=650,
-    height=400
-).configure_view(
-    strokeWidth=0
-)
-st.altair_chart(fig1, use_container_width=False)
-#fig = plt.scatter(x= temp_df["Age"], y= temp_df["Python_score"])
-#st.pyplot(fig) #display the plot
-'''
