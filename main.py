a687ef97b84a06af0cfd2d0e445f0d1e8fdfd1c0
KiriKoppelgaard <43064671+KiriKoppelgaard@users.noreply.github.com>
2022-06-06 14:46:13 +0200

    Adding navigation

--- main.py
+++ main.py
@@ -8,26 +8,26 @@ from page5 import *
 st.set_page_config(layout = "wide")
 
 #define style
-local_css("styles.css")
+#local_css("styles.css")
 
-option_names = ["page1", "page2", "page5"]
+#option_names = ["page1", "page2", "page5"]
 
 placeholder = st.empty()
 
 next = st.button("Next/save")
 
+if 'page' not in st.session_state: 
+   st.session_state["page"] ='page1'
+
 if next:
-   if st.session_state["radio_option"] == 'page1':
-        st.session_state.radio_option = 'page2'
-   elif st.session_state["radio_option"] == 'page2':
-        st.session_state.radio_option = 'page5'
-   else: 
-       st.session_state["radio_option"] = 'page1'
+   if st.session_state["page"] == 'page1':
+        st.session_state["page"] = 'page2'
+   elif st.session_state["page"] == 'page2':
+        st.session_state["page"] = 'page5'
+   elif st.session_state["page"] == 'page5':
+        st.session_state["page"] = 'page1'
 
-with st.expander('Navigate'): 
-   option = st.radio("Pick an option", option_names , key="radio_option")
-
-if option == 'page1':
+if st.session_state["page"] == 'page1':
    with placeholder.container(): 
       temp_df, radar_data, education_rank = page1()
       #image_files=[]
@@ -37,11 +37,11 @@ if option == 'page1':
       #   temp_df['ano_image'] = image_files    
       #   temp_df.to_csv("Data/applicants-from-page-1.csv")
       
-elif option == 'page2':
+elif st.session_state["page"] == 'page2':
    with placeholder.container(): 
       yes_candidates = page2(candidates)
       df = candidates.iloc[yes_candidates]
       df.to_csv('Data/yes_candidates.csv') 
-elif option == 'page5':
+elif st.session_state["page"] == 'page5':
    with placeholder.container(): 
       page5()
