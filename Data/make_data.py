839e03c30f48e66080a3d31029463f1bec7596a1
KiriKoppelgaard <43064671+KiriKoppelgaard@users.noreply.github.com>
2022-06-08 20:27:59 +0200

    changing alias to ID

--- Data/make_data.py
+++ Data/make_data.py
@@ -32,7 +32,7 @@ english_proficiency = ['No Proficiency', 'Limited', 'Professional', 'Advanced', 
 # create a csv file named to contain the dataset
 with open('Data/applicants' + str(nr_applicants) + '.csv', 'w', newline ='') as f: 
     file = csv.writer(f)
-    file.writerow(['Name', 'Age', 'Sex', 'Python Score', 'Education Level', 'Education', 'Faculty', 'Workfields', "Years Experience", "image", "Personality Profiles", "Strength", "Text1", "Text2", "English Proficiency", "GPA", 'Skills', 'Motivation Letter', 'SQL Score', 'Alias'])
+    file.writerow(['Name', 'Age', 'Sex', 'Python Score', 'Education Level', 'Education', 'Faculty', 'Workfields', "Years Experience", "image", "Personality Profiles", "Strength", "Text1", "Text2", "English Proficiency", "GPA", 'Skills', 'Motivation Letter', 'SQL Score', 'ID'])
       
     # generate applicant data from random combinations of variables
     for i in range(nr_applicants):
